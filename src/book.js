96c9dc26f22b0afa827a90832ecae36c8778d1c3
Rosa The Dev <richardjrosaserrano@gmail.com>
2021-10-13 21:54:58 -0400

    Make a buildMainCharacter function and it should create a chracter object

--- src/book.js
+++ src/book.js
@@ -1,9 +1,16 @@
 function createTitle(title) {
   return `The ${title}`
 }
-function buildMainCharacter() {
-  
+
+function buildMainCharacter(name, age,pronouns) {
+   var mainCharacter = {
+  name: name,
+  age: age,
+  pronouns: pronouns
+  }
+  return mainCharacter;
 }
+
 
 module.exports = {
   createTitle,
