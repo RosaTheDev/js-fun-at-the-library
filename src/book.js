eee284987e0e03f45e9514f592b92c6f0ee82302
Rosa The Dev <richardjrosaserrano@gmail.com>
2021-10-13 21:31:04 -0400

    Make a buildMainCharacter function

--- src/book.js
+++ src/book.js
@@ -1,10 +1,13 @@
 function createTitle(title) {
   return `The ${title}`
 }
+function buildMainCharacter() {
+  
+}
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
+  buildMainCharacter,
   // saveReview,
   // calculatePageCount,
   // writeBook,
